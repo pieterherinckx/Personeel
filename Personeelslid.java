1e256d43b4bd4e0400916edf42d3daee2e42e320
Pieter Herinckx <pieterherinckx@katienen.lan>
2019-03-01 13:53:32 +0100

    Create Personeelslid.java

---
+++ Personeelslid.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Personeelslid here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Personeelslid
+{
+    private String naam;
+    
+    public Personeelslid(String naam){
+        this.naam = naam;
+    }
+    
+    public String getNaam(){
+        return naam;
+    }
+    
+    public int getLoon(){
+        return 0;
+    }
+}
