498915f7dde1b7a21486cef1a346b6398aefebc4
pieterherinckx <pieterherinckx@atheneumtienen.be>
2024-03-28 11:50:33 +0100

    Update Bediende.java

--- Bediende.java
+++ Bediende.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Bediende here.
+ * class Bediende - geef hier een beschrijving van deze class
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (jouw naam)
+ * @version (versie nummer of datum)
  */
 public class Bediende extends Personeelslid
 {
